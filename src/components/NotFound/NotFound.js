a2778fca7870f63a72ddbfa007fe1167c545a28d
dimorl02 <dimorl02@gmail.com>
2024-02-18 19:12:11 +0300

    редирект страницы 404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,15 +1,19 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Navigate, useNavigate } from "react-router-dom"
 import "./NotFound.css"
 
 function NotFound() {
+  const navigate = useNavigate();
+  function goNavigate(){
+    navigate(-2)
+  }
   return (
     <section className="not-found">
       <h2 className="not-found__title">404</h2>
       <p className="not-found__subtitle">Страница не найдена</p>
-      <Link to="/" className="not-found__button">
+      <button onClick={goNavigate} className="not-found__button">
         Назад
-      </Link>
+      </button>
     </section>
   )
 }
